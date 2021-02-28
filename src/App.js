7f1561da7c145cdc490133470aa85a2ea90ea1e2
Anand <anand.bhopale@gmail.com>
2021-02-28 15:12:57 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a href="hello.html" src="hello.html">
+          Hello World. Welcome to react components.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
